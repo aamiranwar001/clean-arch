2bee75d153da897c8232de501c4821202d0d0bac
Aamir Anwar <aamiranwar001@gmail.com>
2024-02-27 02:04:32 +0500

    minor changes

--- src/Contour.CleanArchitecture.Core/ProjectAggregate/Project.cs
+++ src/Contour.CleanArchitecture.Core/ProjectAggregate/Project.cs
@@ -8,9 +8,9 @@ public class Project : EntityBase, IAggregateRoot
 {
   public string Name { get; private set; }
 
-  private readonly List<ToDoItem> _items = new();
+  private readonly List<ToDoItem> _items = [];
   public IEnumerable<ToDoItem> Items => _items.AsReadOnly();
-  public ProjectStatus Status => _items.All(i => i.IsDone) ? ProjectStatus.Complete : ProjectStatus.InProgress;
+  public ProjectStatus Status => _items.TrueForAll(i => i.IsDone) ? ProjectStatus.Complete : ProjectStatus.InProgress;
 
   public PriorityStatus Priority { get; }
 
